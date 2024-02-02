21dcd090f12afc675f9b21d012c75f818c4e08ef
jellopy <parnupat.j@gmail.com>
2024-02-03 01:44:17 +0700

    use svg-icon

---
+++ src/components/svgIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :style="`width: auto; height: ${height};`" :is="dynamicComponent" />
+</template>
+
+<script lang="ts">
+import { defineComponent,defineAsyncComponent } from 'vue'
+export default defineComponent({
+  name: 'SvgIcon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    },
+  },
+  computed: {
+    dynamicComponent() {
+      const name = this.name;
+      return defineAsyncComponent(() => import(`../assets/${name}.svg`));
+    },
+  }
+})
+</script>
