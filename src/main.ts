e7be2ac59df1ad8a2423c9c7187d9d5c2be45724
jellopy <parnupat.j@gmail.com>
2024-02-03 09:44:09 +0700

    import css/element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import router from './routes'
 import App from './App.vue'
+import 'element-plus/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
 const app = createApp(App)
